72a492b36c278f1f0afbe5580212d572d9b9484d
icyJoseph <sephxd1234@gmail.com>
2018-08-25 09:41:36 +0200

    refactor: use axios for XHR

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,45 +1,25 @@
-const request = require("request");
+const axios = require("axios");
 
 module.exports.geocodeAddress = address => {
-  return new Promise((resolve, reject) => {
-    const encodedAddress = encodeURIComponent(address);
-    const url = `http://www.mapquestapi.com/geocoding/v1/address?key=${
-      process.env.GEOLOCATION_KEY
-    }&location=${encodedAddress}`;
+  const encodedAddress = encodeURIComponent(address);
+  const url = `http://www.mapquestapi.com/geocoding/v1/address?key=${
+    process.env.GEOLOCATION_KEY
+  }&location=${encodedAddress}`;
 
-    request(
-      {
-        url,
-        json: true
-      },
-      (error, res, body) => {
-        //console.log(body.results[0].locations);
-
-        if (error) {
-          //   console.log(error);
-          const message = "Could not connect to the Geolocation API.";
-          return reject(message);
-        }
-        if (
-          body.results[0].locations.length === 0 ||
-          !body.results[0].locations[0].street
-        ) {
-          const invalid = "Invalid Address";
-          return reject(invalid);
-        }
-
-        const [
-          { street, postalCode, adminArea5, adminArea1, latLng }
-        ] = body.results[0].locations;
-
-        return resolve({
-          street,
-          postalCode,
-          city: adminArea5,
-          country: adminArea1,
-          ...latLng
-        });
-      }
-    );
+  return axios.get(url).then(res => {
+    const {
+      street,
+      postalCode,
+      adminArea5,
+      adminArea1,
+      latLng
+    } = res.data.results[0].locations[0];
+    return {
+      street,
+      postalCode,
+      city: adminArea5,
+      country: adminArea1,
+      ...latLng
+    };
   });
 };
--- weather/weather.js
+++ weather/weather.js
@@ -1,29 +1,11 @@
-const request = require("request");
+const axios = require("axios");
 
 module.exports.getWeather = (lat, lng) => {
-  return new Promise((resolve, reject) => {
-    const url = `https://api.sdarksky.net/forecast/${
-      process.env.WEATHER_KEY
-    }/${lat},${lng}?units=si`;
+  const url = `https://api.darksky.net/forecast/${
+    process.env.WEATHER_KEY
+  }/${lat},${lng}?units=si`;
 
-    request(
-      {
-        url,
-        json: true
-      },
-      (error, res, body) => {
-        if (error) {
-          const message = "Could not connect to the Forecast API.";
-          return reject(message);
-        }
-        if (res.statusCode !== 200) {
-          return reject(`HTTP request error ${res.statusCode}`);
-        }
-        if (body.error) {
-          return reject(body.error);
-        }
-        return resolve(body.currently);
-      }
-    );
+  return axios.get(url).then(res => {
+    return res.data.currently;
   });
 };
